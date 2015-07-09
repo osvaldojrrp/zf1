66b1fab0f757734636b70473f73e7eabf7bd0e2d
Osvaldo Junior Reis Paulino <osvaldo.jr.paulino@gmail.com>
2015-07-08 22:26:48 -0300

    Configuração  - bootstrap.php  - index.php

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+// Define o caminho do diretório application
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+
+// Define o ambiente da aplicação
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+// Certifica-se que a biblioteca está no include_path
+set_include_path(implode(PATH_SEPARATOR, array(realpath(APPLICATION_PATH . '/../library'), get_include_path(),)));
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+
+// Cria a application, o bootstrap, and run
+$application = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+
+$application->bootstrap()
+    ->run();
